c41f1766b3e884b6dec8c932499486eb66982dda
timwlee <timothyl@adobe.com>
2024-10-15 17:09:54 -0700

    Add links block

---
+++ blocks/links/links.js
@@ -0,0 +1,26 @@
+
+export default function decorate(block) {
+  const children = block.children;
+
+  [...children].forEach((child) => { 
+    child.classList.add('link-item');
+    const div = child.querySelector(':scope > div');
+
+    const consoleLink = child.querySelector('a').title;
+    console.log("console: ", consoleLink);
+
+    if (consoleLink.toLowerCase() === 'console') {
+      child.classList.add('console-btn');
+    }
+
+    const titleText = div.querySelector('p:first-child').textContent;
+    const link = div.querySelector('a');
+
+    const newLink = document.createElement('a');
+    newLink.href = link.href;
+    newLink.title = titleText;
+    newLink.textContent = titleText;
+
+    div.replaceWith(newLink);
+  });
+}
